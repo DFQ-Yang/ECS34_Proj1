88672fb819ad6f231ef090b1a1b68f729edf12e0
DFQ-Yang <zirui522@gmail.com>
2026-01-19 02:15:30 -0800

    gtest for Upper function finished

--- testsrc/StringUtilsTest.cpp
+++ testsrc/StringUtilsTest.cpp
@@ -10,7 +10,21 @@ TEST(StringUtilsTest, Capitalize){
 }
 
 TEST(StringUtilsTest, Upper){
-    
+    std::string uppered;
+    //Test empty and blank
+    uppered = StringUtils::Upper("");
+    EXPECT_EQ(uppered, "");
+    uppered = StringUtils::Upper("           ");
+    EXPECT_EQ(uppered, "           ");
+
+    //Test with numbers
+    uppered = StringUtils::Upper("Only 90 more MB remaining, what can I do....");
+    EXPECT_EQ(uppered, "ONLY 90 MORE MB REMAINING, WHAT CAN I DO....");
+
+    //Test normal
+    uppered = StringUtils::Upper("If it is not the problem of WSL");
+    EXPECT_EQ(uppered, "IF IT IS NOT THE PROBLEM OF WSL");
+
 }
 
 TEST(StringUtilsTest, Lower){
