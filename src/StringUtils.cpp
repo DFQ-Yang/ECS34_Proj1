fc813c117e24b6e423bc035ab6c099d208435bf0
DFQ-Yang <zirui522@gmail.com>
2026-01-19 02:03:29 -0800

    feat: captialize function finished and tests passed

--- src/StringUtils.cpp
+++ src/StringUtils.cpp
@@ -8,8 +8,34 @@ std::string Slice(const std::string &str, ssize_t start, ssize_t end) noexcept{
 }
 
 std::string Capitalize(const std::string &str) noexcept{
-    // Replace code here
-    return "";
+    std::string res = "";
+    char ch;
+    //Check empty str
+    if(str.empty()) return str;
+
+    //Modify first
+    if(str.at(0) >= 97 && str.at(0) <= 122){
+        ch = str.at(0) - 32;
+        res += ch;
+    }
+    else{
+        res += str.at(0);
+    }
+
+    //Modify rest
+    for(int i = 1; i < str.length(); i++){
+        //If upperCase change it to lowerCase
+        if(str.at(i) >= 65 && str.at(i) <= 90){
+            ch = str.at(i) + 32;
+            res += ch;
+        }
+        //else add it directly into res
+        else{
+            res += str.at(i);
+        }
+    }
+
+    return res;
 }
 
 std::string Upper(const std::string &str) noexcept{
